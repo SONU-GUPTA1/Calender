1026484d428afc7d025ce5830bd6c28b37e1acf9
SONU-GUPTA1 <109684425+SONU-GUPTA1@users.noreply.github.com>
2022-10-23 17:59:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,66 @@
+// var dt = new Date();
+// function RenderDate() {
+//   dt.setDate(1);
+//   var day = dt.getDay();
+//   console.log(dt.getDay());
+//   var endDate = new Date(dt.getFullYear(), dt.getMonth() + 1, 0).getDate();
+
+//   var prevDate = new Date(dt.getFullYear(), dt.getMonth(), 0).getDate();
+
+//   var today = new Date();
+//   console.log(today);
+//   var months = [
+//     "January",
+//     "February",
+//     "March",
+//     "April",
+//     "May",
+//     "June",
+//     "July",
+//     "August",
+//     "September",
+//     "October",
+//     "November",
+//     "December",
+//   ];
+//   document.getElementById("date_str").innerHTML = dt.toDateString();
+//   document.getElementById("month").innerHTML = months[dt.getMonth()];
+//   var cells = "";
+
+//   for (x = day; x > 0; x--) {
+//     cells += "<div class='prev_date' >" + (prevDate - x + 1) + "</div>";
+//   }
+
+//   for (i = 1; i <= endDate; i++) {
+//     if (i == today.getDay() && dt.getMonth() == today.getMonth()) {
+//       cells += "<div class='today'>" + i + "</div>";
+//     } else {
+//       cells += "<div>" + i + "</div>";
+//     }
+//   }
+//   document.getElementsByClassName("days")[0].innerHTML = cells;
+// }
+// function moveDate(para) {
+//   if (para == "prev") {
+//     dt.setMonth(dt.getMonth() - 1);
+//   } else if (para == "next") {
+//     dt.setMonth(dt.getMonth() + 1);
+//   }
+//   RenderDate();
+// }
+
+//____________________-
+
 var dt = new Date();
 function RenderDate() {
   dt.setDate(1);
   var day = dt.getDay();
-  console.log(dt.getDay());
   var endDate = new Date(dt.getFullYear(), dt.getMonth() + 1, 0).getDate();
 
-  var prevDate = new Date(dt.getFullYear(), dt.getMonth(), 0).getDate();
+  var prevDate = new Date(dt.getFullYear(), dt.getMonth() + 1, 0).getDate();
 
   var today = new Date();
-  console.log(today);
+
   var months = [
     "January",
     "February",
@@ -17,29 +69,34 @@ function RenderDate() {
     "May",
     "June",
     "July",
-    "August",
+    "Augest",
     "September",
     "October",
     "November",
     "December",
   ];
+
   document.getElementById("date_str").innerHTML = dt.toDateString();
+
   document.getElementById("month").innerHTML = months[dt.getMonth()];
+
   var cells = "";
 
   for (x = day; x > 0; x--) {
-    cells += "<div class='prev_date' >" + (prevDate - x + 1) + "</div>";
+    cells += "<div class='pre_date'>" + (prevDate - x + 1) + "</div>";
   }
 
   for (i = 1; i <= endDate; i++) {
-    if (i == today.getDay() && dt.getMonth() == today.getMonth()) {
-      cells += "<div class='today'>" + i + "</div>";
+    if (i == today.getDate() && dt.getMonth() == today.getMonth()) {
+      cells += "<div class ='today'>" + i + "</div>";
     } else {
       cells += "<div>" + i + "</div>";
     }
   }
+
   document.getElementsByClassName("days")[0].innerHTML = cells;
 }
+
 function moveDate(para) {
   if (para == "prev") {
     dt.setMonth(dt.getMonth() - 1);
@@ -48,3 +105,17 @@ function moveDate(para) {
   }
   RenderDate();
 }
+var events = [
+  { Date: new Date(dt.getDay()), Title: "Doctor appointment at 3:25pm." },
+  {
+    Date: new Date(dt.getDay()),
+    Title: "New Garfield movie comes out!",
+    Link: "https://garfield.com",
+  },
+  {
+    Date: new Date(dt.getDay()),
+    Title: "25 year anniversary",
+    Link: "https://www.google.com.au/#q=anniversary+gifts",
+  },
+];
+
